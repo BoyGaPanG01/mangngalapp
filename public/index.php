eb9461c6ecd26538f9f3fb7abb59c8d8a29af818
BoyGaPanG01 <justinegamo04@gmail.com>
2026-01-20 15:18:14 +0800

    Create public/index.php as the main entry point

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+// Main entry point for the application
+
+// Set timezone
+date_default_timezone_set('UTC');
+
+// Output the current date and time
+echo 'Current Date and Time (UTC): ' . date('Y-m-d H:i:s');
+?>
